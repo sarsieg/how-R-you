a0941576b42bccd0a3b9fba2eb9cdd4bc2016b93
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-24 22:58:58 -0500

    fixed spelling eroor

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const { authMiddleware } = require("./utils/auth");
 
-const { ApolloServer } = require('apollo-server-express');
+const { ApollowServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
