fdf89f53d8e67364eee42e6ef046a9199c432ddc
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-24 22:51:08 -0500

    imported typedefs and resolvers as well as defined local port and express server

--- server/server.js
+++ server/server.js
@@ -4,3 +4,8 @@ const { authMiddleware } = require("./utils/auth");
 
 const { ApollowServer } = require('apollo-server-express');
 
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
