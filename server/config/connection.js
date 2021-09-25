acf7c322651e184362dc116a9f84c9e570d2f0ae
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-25 00:18:39 -0500

    created connection to database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/howryou', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
