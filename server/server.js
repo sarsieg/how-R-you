ee363ea1c139c01f53dba38769e17be253955bce
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-24 23:23:48 -0500

    integrated apollo server with express application as middleware, parse incoming string or array data and parse incoming json data

--- server/server.js
+++ server/server.js
@@ -15,3 +15,8 @@ const server = new ApolloServer({
     resolvers,
     context: authMiddleware
 });
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
