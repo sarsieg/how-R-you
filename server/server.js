5cb5f786c9842140c27eb61d95c981ee630ec2cb
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-24 23:01:21 -0500

    fixing error

--- server/server.js
+++ server/server.js
@@ -2,5 +2,10 @@ const express = require("express");
 const path = require("path");
 const { authMiddleware } = require("./utils/auth");
 
-const { ApollowServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
