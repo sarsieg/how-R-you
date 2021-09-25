e71aa1c55a55674dc5cf1415555d62b592f3711e
Steve Siegel <stevegabrielsiegel@gmail.com>
2021-09-25 11:33:33 -0500

    created function to shorten the names of the month for the timestamps

--- server/utils/dateformat.js
+++ server/utils/dateformat.js
@@ -16,3 +16,24 @@ const addDate = date => {
     
     return dateString;
 };
+
+module.exports = (
+    timestamp,
+    { monthLength = 'short', dateSuffix = true } = {}
+) => {
+
+    const months = {
+        0: monthLength === 'short' ? 'Jan' : 'January',
+        1: monthLength === 'short' ? 'Feb' : 'February',
+        2: monthLength === 'short' ? 'Mar' : 'March',
+        3: monthLength === 'short' ? 'Apr' : 'April',
+        4: monthLength === 'short' ? 'May' : 'May',
+        5: monthLength === 'short' ? 'Jun' : 'June',
+        6: monthLength === 'short' ? 'jul' : 'July',
+        7: monthLength === 'short' ? 'Aug' : 'August',
+        8: monthLength === 'short' ? 'Sep' : 'September',
+        9: monthLength === 'short' ? 'Oct' : 'October',
+        10: monthLength === 'short' ? 'Nov' : 'November',
+        11: monthLength === 'short' ? 'Dec' : 'December',
+    };
+}
