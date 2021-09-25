4e9e9d4512c6d108fe574cb604c59eac34243990
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-24 23:52:29 -0500

    added function to connect to the server in server.js file

--- server/server.js
+++ server/server.js
@@ -29,3 +29,10 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhos:${PORT}${server.graphqlPath}`);
+    });
+});
