0919ac7bf3e0a56f14ad1375451bd23f4836c7a5
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-09-26 21:03:49 -0500

    cleaning up code in server.js file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const path = require("path");
-const { authMiddleware } = require("./utils/auth");
-
 const { ApolloServer } = require('apollo-server-express');
+const path = require("path");
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require("./utils/auth");
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
