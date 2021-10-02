7d70e42b671f31f1d9ffa7ec7deb9cde4f4f78fa
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-10-01 20:28:21 -0500

    fixed some more wording and made post and comment buttons smaller

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -67,7 +67,7 @@ const CommentForm = ({ thoughtId }) => {
                     </div>
 
                     <div className="col-12 col-lg-3">
-                        <button className="btn btn-primary btn-block py-3" type="submit">
+                        <button className="btn btn-primary btn-block" type="submit">
                             Add Comment
                         </button>
                     </div>
